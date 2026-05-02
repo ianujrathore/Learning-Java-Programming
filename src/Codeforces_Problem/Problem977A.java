0abb0f7032372b7c0d4faed254356b629df62d17
Annu <ianujrathore@gmail.com>
2026-05-02 22:55:47 +0530

    Add Problem977A.java for Codeforces problem solution

---
+++ src/Codeforces_Problem/Problem977A.java
@@ -0,0 +1,22 @@
+package Codeforces_Problem;
+
+import java.util.Scanner;
+
+public class Problem977A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        for (int i = 0; i < k; i++) {
+            if (n % 10 == 0) {
+                n /= 10;
+            } else {
+                n -= 1;
+            }
+        }
+        
+        System.out.println(n);
+    }
+}
