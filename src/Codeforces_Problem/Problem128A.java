13a3df03d48faae107618d335c91db882fa0f833
Annu <ianujrathore@gmail.com>
2026-05-03 23:02:04 +0530

    Add Problem128A solution for Codeforces

---
+++ src/Codeforces_Problem/Problem128A.java
@@ -0,0 +1,28 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Problem128A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] scores = new int[n];
+        for (int i = 0; i < n; i++) {
+            scores[i] = sc.nextInt();
+        }
+
+        int threshold = scores[k - 1];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (scores[i] >= threshold && scores[i] > 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
