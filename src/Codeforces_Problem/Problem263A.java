9e1cf569010732f1ce55e5b476ec27d289c01d1d
Annu <ianujrathore@gmail.com>
2026-05-06 20:53:52 +0530

    Add Problem263A solution for Codeforces

---
+++ src/Codeforces_Problem/Problem263A.java
@@ -0,0 +1,23 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = 0, col = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                int x = sc.nextInt();
+                if (x == 1) {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        int moves = Math.abs(row - 2) + Math.abs(col - 2);
+        System.out.println(moves);
+    }
+}
