2904f357b6ba74b2831e38ab4bf931612e309baf
Annu <ianujrathore@gmail.com>
2026-05-10 22:25:51 +0530

    Add Problem281A.java for Codeforces problem solution

---
+++ src/Codeforces_Problem/Problem281A.java
@@ -0,0 +1,15 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        System.out.println(
+            Character.toUpperCase(s.charAt(0)) + s.substring(1)
+        );
+    }
+}
