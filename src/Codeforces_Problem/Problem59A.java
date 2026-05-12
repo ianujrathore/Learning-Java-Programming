aaebed70e2c436b2b8eb7f4a710c697105ee968d
Annu <ianujrathore@gmail.com>
2026-05-12 22:38:44 +0530

    Add solution for Codeforces Problem 59A
    Implement a solution for Codeforces Problem 59A that converts a string to uppercase or lowercase based on the count of uppercase and lowercase letters.

---
+++ src/Codeforces_Problem/Problem59A.java
@@ -0,0 +1,26 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        
+        int upper = 0, lower = 0;
+
+        for (char c : s.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                upper++;
+            } else {
+                lower++;
+            }
+        }
+
+        if (upper > lower) {
+            System.out.println(s.toUpperCase());
+        } else {
+            System.out.println(s.toLowerCase());
+        }
+    }
+}
