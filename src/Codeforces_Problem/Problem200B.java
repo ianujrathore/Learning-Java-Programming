68c7b68cdc1a0df4c7145f3f3953df3dc8a3de4d
Annu <ianujrathore@gmail.com>
2026-05-17 20:12:09 +0530

    Add Problem200B.java for Codeforces problem solution

---
+++ src/Codeforces_Problem/Problem200B.java
@@ -0,0 +1,18 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Problem200B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += sc.nextInt();
+        }
+
+        System.out.println(sum / n);
+    }
+}
