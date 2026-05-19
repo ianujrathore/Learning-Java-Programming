4fb9fcd5fef0a40c555e27315b8dc9d9bae15372
Annu <ianujrathore@gmail.com>
2026-05-19 23:42:08 +0530

    Add Problem266B.java for Codeforces problem solution

---
+++ src/Codeforces_Problem/Problem266B.java
@@ -0,0 +1,26 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Problem266B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        char[] s = sc.next().toCharArray();
+
+        while (t-- > 0) {
+            for (int i = 0; i < n - 1; i++) {
+                if (s[i] == 'B' && s[i + 1] == 'G') {
+                    char temp = s[i];
+                    s[i] = s[i + 1];
+                    s[i + 1] = temp;
+                    i++;
+                }
+            }
+        }
+
+        System.out.println(new String(s));
+    }
+}
