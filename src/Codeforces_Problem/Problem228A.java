c9f964c520904b6c08e8a4ae000cb4cced36bb03
Annu <ianujrathore@gmail.com>
2026-05-25 22:35:26 +0530

    Add Problem228A.java for Codeforces problem solution

---
+++ src/Codeforces_Problem/Problem228A.java
@@ -0,0 +1,17 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Problem228A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < 4; i++) {
+            set.add(sc.nextInt());
+        }
+
+        System.out.println(4 - set.size());
+    }
+}
