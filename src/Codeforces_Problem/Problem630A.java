01943eaec98d38c15c266de10feecf5d0518ec48
ianujrathore <ianujrathore@gmail.com>
2026-06-03 22:10:05 +0530

    Codeforces Problem

---
+++ src/Codeforces_Problem/Problem630A.java
@@ -0,0 +1,11 @@
+package Codeforces_Problem;
+
+import java.util.Scanner;
+
+public class Problem630A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.next();
+        System.out.println(25);
+    }
+}
