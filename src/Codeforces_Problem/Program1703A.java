7785d7b14a3504594947f109636e2da0e5f5efbf
Annu <ianujrathore@gmail.com>
2026-06-08 21:21:12 +0530

    Add Program1703A for Codeforces problem

---
+++ src/Codeforces_Problem/Program1703A.java
@@ -0,0 +1,19 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Problem1703A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next().toLowerCase();
+            if (s.equals("yes")) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
