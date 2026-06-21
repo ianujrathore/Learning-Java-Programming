9a530f2469afdb2f3f11a91b2741515edbd5f3d4
Annu <ianujrathore@gmail.com>
2026-06-21 20:42:48 +0530

    Add Program732A.java for Codeforces problem solution

---
+++ src/Codeforces_Problem/Program732A.java
@@ -0,0 +1,21 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Problem732A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int r = sc.nextInt();
+
+        int n = 1;
+        while (true) {
+            int x = (k * n) % 10;
+            if (x == 0 || x == r) {
+                System.out.println(n);
+                break;
+            }
+            n++;
+        }
+    }
+}
