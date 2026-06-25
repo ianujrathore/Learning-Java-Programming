fa978d0243cd52e60f852f4723500f8e120145fb
Annu <ianujrathore@gmail.com>
2026-06-25 23:32:05 +0530

    Add Problem1857A solution for Codeforces

---
+++ src/Codeforces_Problem/Problem1857A.java
@@ -0,0 +1,22 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Problem1857A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                sum += sc.nextInt();
+            }
+
+            System.out.println(sum % 2 == 0 ? "YES" : "NO");
+        }
+    }
+}
