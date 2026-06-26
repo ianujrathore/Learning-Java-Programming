2723370bce51186329035f02086728fab7d5b5b2
Annu <ianujrathore@gmail.com>
2026-06-26 23:56:38 +0530

    Add Problem2009A solution for Codeforces
    Implement a solution for Codeforces Problem 2009A that calculates the difference between two integers for multiple test cases.

---
+++ src/Codeforces_Problem/Problem2009A.java
@@ -0,0 +1,19 @@
+package Codeforces_Problem;
+
+import java.util.Scanner;
+
+public class Problem2009A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(b - a);
+        }
+
+        sc.close();
+    }
+}
