24f89d1e409117e82cf7bb9d82f342a792d1c6b9
Annu <ianujrathore@gmail.com>
2026-07-20 23:18:37 +0530

    Add Problem1873A solution for specific string checks

---
+++ src/Codeforces_Problem/Problem1873.java
@@ -0,0 +1,19 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Problem1873A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+            if (s.equals("abc") || s.equals("acb") || s.equals("bac") || s.equals("cba")) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
