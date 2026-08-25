5dd24560ab09a093b139b5365cd75625864b1dd8
Annu <ianujrathore@gmail.com>
2026-08-25 22:29:04 +0530

    Add Problem1454A solution for Codeforces

---
+++ src/Codeforces_Problem/problem1454A.java
@@ -0,0 +1,22 @@
+package Codeforces_Problem;
+
+import java.util.*;
+
+public class Problem1454A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = (n + 1) / 2;
+
+            for (int i = 1; i <= n; i++) {
+                System.out.print((i + k - 1) % n + 1 + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
