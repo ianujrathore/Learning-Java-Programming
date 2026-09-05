5d7cac461f058e17d6cc53a4e2b9d78845ec00e5
ianujrathore <ianujrathore@gmail.com>
2026-09-05 13:05:58 +0530

    Name Corrections

--- src/Codeforces_Problem/Problem2149A.java
+++ src/Codeforces_Problem/Problem2149A.java
@@ -5,17 +5,22 @@ import java.util.*;
 public class Problem2149A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int t = sc.nextInt();
 
         while (t-- > 0) {
             int n = sc.nextInt();
-            int neg = 0;
+            int zeros = 0;
+            int negatives = 0;
 
             for (int i = 0; i < n; i++) {
-                if (sc.nextInt() < 0) neg++;
+                int x = sc.nextInt();
+
+                if (x == 0) zeros++;
+                if (x == -1) negatives++;
             }
 
-            System.out.println((neg + 1) / 2);
+            System.out.println(zeros + (negatives % 2) * 2);
         }
     }
 }
